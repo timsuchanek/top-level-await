d755044fa48f593b48aa30dee6c51feb563f8259
timsuchanek <tim.suchanek@gmail.com>
2020-04-17 08:56:08 +0200

    init

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+declare const x: null;
+export { x };
---
+++ index.ts
@@ -0,0 +1,13 @@
+// import {PrismaClient} from '@prisma/client'
+
+// const prisma = new PrismaClient()
+
+// const users = await prisma.user.findMany()
+
+async function doSomething() {
+  return null
+}
+
+const x = await doSomething()
+
+export { x }
